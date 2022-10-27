c9a1ecaeed1b7334fe52a874ca43e99d0d635c38
sebousan <sebousan@gmail.com>
2022-10-27 09:29:38 +0200

    statutes

--- app/collections/partials/statutes.js
+++ app/collections/partials/statutes.js
@@ -1,10 +1,24 @@
+// export const statutes = {
+//     name: 'statutes',
+//     label: 'Satuts',
+//     widget: 'relation',
+//     collection: 'statutes',
+//     search_fields: ['title'],
+//     value_field: '{{slug}}',
+//     display_fields: ['title'],
+//     required: false
+// }
+
 export const statutes = {
     name: 'statutes',
     label: 'Satuts',
-    widget: 'relation',
-    collection: 'statutes',
-    search_fields: ['title'],
-    value_field: '{{slug}}',
-    display_fields: ['title'],
+    widget: 'select',
+    options: [
+        { label: 'Associé', value: 'associate' },
+        { label: 'Collaborateur', value: 'collaboratorr' },
+        { label: 'Juriste', value: 'jurist' },
+        { label: 'Secrétaire', value: 'secretary' }
+        { label: 'Stagiaire', value: 'trainee' },
+    ],
     required: false
 }
