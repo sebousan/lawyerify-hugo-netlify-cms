14371548602c06e1645ccb93b35cb83137f748dc
sebousan <sebousan@gmail.com>
2023-01-11 20:14:48 +0100

    statutes

--- app/fields/statutes.js
+++ app/fields/statutes.js
@@ -1,24 +1,9 @@
-// export const statutes = {
-//     name: 'statutes',
-//     label: 'Satuts',
-//     widget: 'relation',
-//     collection: 'statutes',
-//     search_fields: ['title'],
-//     value_field: '{{slug}}',
-//     display_fields: ['title'],
-//     required: false
-// }
+import { availableSatutes } from '../settings/available-statutes.js';
 
 export const statutes = {
     name: 'statutes',
     label: 'Satuts',
     widget: 'select',
-    options: [
-        { label: 'Associé', value: 'associate' },
-        { label: 'Collaborateur', value: 'collaborator' },
-        { label: 'Juriste', value: 'jurist' },
-        { label: 'Secrétaire', value: 'secretary' },
-        { label: 'Stagiaire', value: 'trainee' }
-    ],
+    options: availableSatutes,
     required: true
 }
---
+++ app/settings/available-statutes.js
@@ -0,0 +1,8 @@
+export const availableSatutes = [
+    { label: 'Associé', value: 'associate' },
+    { label: 'Collaborateur', value: 'collaborator' },
+    { label: 'Conseil', value: 'counsel' },
+    { label: 'Juriste', value: 'jurist' },
+    { label: 'Secretaire', value: 'secretary' },
+    { label: 'Stagiaire', value: 'trainee' }
+]
