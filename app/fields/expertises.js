3e08d3d27fdd5749c3d235b6e28eccb7661df41d
sebousan <sebousan@gmail.com>
2023-01-13 17:13:53 +0100

    expertises fix

--- app/fields/expertises.js
+++ app/fields/expertises.js
@@ -6,6 +6,7 @@ export const expertises = {
     search_fields: ['title'],
     value_field: '{{title}}',
     display_fields: ['title'],
+    multiple: true,
     required: false,
     i18n: 'duplicate'
 }
