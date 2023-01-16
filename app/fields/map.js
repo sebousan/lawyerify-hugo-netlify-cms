86520f0ab35d96f0395c26dc92de0e4fac029ac0
sebousan <sebousan@gmail.com>
2023-01-16 11:32:03 +0100

    map

--- app/fields/map.js
+++ app/fields/map.js
@@ -3,5 +3,7 @@ export const map = {
     label: 'Localisation',
     widget: 'map',
     required: false,
+    default: '{ "type": "Feature", "geometry": { "type": "Point", "coordinates": [2.333333, 48.866667] } }',
+    decimals: 7,
     i18n: 'duplicate'
 }
