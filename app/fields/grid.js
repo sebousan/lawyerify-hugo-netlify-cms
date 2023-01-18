70d2ce67b1cc1792ca44514d17deb72fec069cce
sebousan <sebousan@gmail.com>
2023-01-18 16:23:01 +0100

    fix grid

--- app/fields/grid.js
+++ app/fields/grid.js
@@ -5,7 +5,7 @@ export const grid = {
     options: [
         { label: 'Petit', value: 'small' },
         { label: 'Moyen', value: 'medium' },
-        { label: 'Large', value: 'large' }
+        { label: 'Large', value: 'large' },
         { label: 'Full', value: 'full' }
     ],
     default: 'large',
