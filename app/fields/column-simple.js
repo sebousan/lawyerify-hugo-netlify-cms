05c63ddfe8eff2b59a3218eeaf02fbdcfa6fef5e
sebousan <sebousan@gmail.com>
2023-01-23 14:06:42 +0100

    column

--- app/fields/column-simple.js
+++ app/fields/column-simple.js
@@ -1,10 +1,10 @@
-export const column = { 
+export const column_simple = { 
     name: 'column',
     label: 'Nombre de colonnes',
     widget: 'number',
     min: 1,
-    max: 6,
+    max: 2,
     value_type: 'int',
-    default: 3,
+    default: 1,
     i18n: 'duplicate'
 }
