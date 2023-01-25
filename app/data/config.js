cb1d47d2c57e35b9863bf0cbdcfb4efd223a947b
sebousan <sebousan@gmail.com>
2023-01-25 11:45:29 +0100

    config seo

--- app/data/config.js
+++ app/data/config.js
@@ -14,22 +14,6 @@ const config = {
 
     files: [
 
-        // {
-        //     name: 'statutes',
-        //     label: 'Statuts des personnes',
-        //     file: 'data/statutes.yml',
-        //     i18n: true,
-        //     fields: [
-        //         {
-        //             name: 'statutes',
-        //             label: 'statutes',
-        //             widget: 'list',
-        //             fields: [
-        //                 title
-        //             ]
-        //         }
-        //     ]
-        // },
         {
             name: 'primary',
             label: 'Menu principal',
@@ -79,19 +63,12 @@ const config = {
         {
             name: 'seo',
             label: 'SEO',
-            file: 'config/_default/languages.yml',
+            file: 'data/seo.yml',
             i18n: true,
             fields: [
-                {
-                    name: 'fr',
-                    label: 'Français',
-                    widget: 'object',
-                    fields: [
-                        { name: 'title', label: 'Nom du site', widget: 'string', i18n: true },
-                        description,
-                        image_src
-                    ]
-                }
+                { name: 'title', label: 'Nom du site', widget: 'string', i18n: true },
+                description,
+                image_src
             ]
         },
         {
